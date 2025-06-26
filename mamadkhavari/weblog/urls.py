ee092ed82a974ca4688c25e168be63056f062cc3
Mohammad <m.rahimi.vasteras@gmail.com>
2025-06-26 15:56:47 +0200

    new start

--- mamadkhavari/weblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='index'),
-    path('<int:year>/<int:month>/<int:day>/<str:slug>/', views.post_detail, name='post_detail'),
-    path('contact-us/', views.contact_us, name='contact-us'),
-    #harchi ke bade åath bezani ba / un mishe adresi ke mikhay
-]
